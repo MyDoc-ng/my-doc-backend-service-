aa02846a3da82f926dac4eebf67a231ae7afa90e
theJohnCode <ifeanyichukwujohn70@gmail.com>
2025-02-20 05:43:30 +0100

    restarted the migratioon

--- prisma/migrations/20250219145947_fix_erro/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - The `symptoms` column on the `Appointment` table would be dropped and recreated. This will lead to data loss if there is data in the column.
-
-*/
--- AlterTable
-ALTER TABLE "Appointment" DROP COLUMN "symptoms",
-ADD COLUMN     "symptoms" TEXT[];
