5982ff8da5231b92f89f09910be4bff457c51312
theJohnCode <ifeanyichukwujohn70@gmail.com>
2025-04-01 16:51:32 +0100

    new db

--- prisma/migrations/20250401155109_new_database/migration.sql
+++ prisma/migrations/20250401155109_new_database/migration.sql
@@ -172,6 +172,7 @@ CREATE TABLE "chat_messages" (
     "readAt" TIMESTAMP(3),
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
 
     CONSTRAINT "chat_messages_pkey" PRIMARY KEY ("id")
 );
